195aa8d2e098033df1bfdb0f503ff4fd9f992a8c
Max Wang <maxwang7@gmail.com>
2017-02-05 21:16:54 +0000

    mostly working front-end workflow

--- codalab/apps/web/static/js/worksheet/worksheet_help_button.jsx
+++ codalab/apps/web/static/js/worksheet/worksheet_help_button.jsx
@@ -16,30 +16,91 @@ var HelpButton = React.createClass({
     getInitialState: function() {
         return {
            state: HELP_STATES.INITIAL,
-           messageText: ''
+           message: ''
         };
     },
 
-    clickTransition: function() {
+    sendMessage: function(message) {
+        var data = {
+          message: message
+        };
+
+        var onSuccess = function(data, status, jqXHR) {
+            console.log("success!");
+            console.log(data);
+            this.messageSentTransition(true);
+        }.bind(this);
+
+        var onError = function(jqHXR, status, error) {
+            console.log("error");
+            console.log(error);
+            this.messageSentTransition(false);
+        }.bind(this);
+
+        $.ajax({
+            url: '/rest/help/',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            type: 'POST',
+            success: onSuccess,
+            error: onError
+        });
+    },
+
+    messageSentTransition: function(isSuccess) {
+        var TIMEOUT_TIME = 3000;
+        switch (this.state.state) {
+            case HELP_STATES.SENDING:
+                var nextState;
+                if (isSuccess) {
+                    nextState = HELP_STATES.SUCCESS;
+                } else {
+                    nextState = HELP_STATES.FAILED;
+                }
+
+                this.setState(
+                    {state: nextState},
+                    function() {
+                        setTimeout(this.timerTransition, TIMEOUT_TIME);
+                    }.bind(this)
+                );
+                break;
+            default:
+                console.log("Error: The messageSentTransition() should only be called when the program is in the SENDING state");
+                return;
+        }
+    },
+
+    clickTransition: function(e) {
+        console.log("clickTransition");
         switch (this.state.state) {
             case HELP_STATES.INITIAL:
-                return HELP_STATES.OPEN;
+                this.setState({state: HELP_STATES.OPEN});
+                break;
             case HELP_STATES.OPEN:
-                return HELP_STATES.SENDING;
+                this.sendMessage(this.state.message);
+                this.setState({state: HELP_STATES.SENDING});
+                break;
             default:
-                return state;
+                return;
         }
     },
 
     timerTransition: function() {
         switch (this.state.state) {
             case HELP_STATES.FAILED:
-                return HELP_STATES.OPEN;
+                this.setState({state: HELP_STATES.OPEN});
+                break;
             case HELP_STATES.SUCCESS:
-                return HELP_STATES.INITIAL;
+                this.setState({state: HELP_STATES.INITIAL});
+                break;
             default:
-                return state;
+                return;
         }
+    },
+
+    onMessageChange: function(e) {
+        this.setState({message: e.target.value});
     },
 
     helpButtonIcon: function() {
@@ -61,6 +122,7 @@ var HelpButton = React.createClass({
     },
 
     render: function() {
+      // TODO refactor into less
       var containerStyle = {
           position: 'fixed',
           bottom: '50px',
@@ -68,7 +130,7 @@ var HelpButton = React.createClass({
           zIndex: '1'
       };
 
-      var messageStyle = {
+      var helpButtonStyle = {
           width: '50px',
           height: '50px',
           borderRadius: '25px',
@@ -83,23 +145,36 @@ var HelpButton = React.createClass({
           boxShadow: 'none'
       };
 
-      var helpButtonStyle = {
+
+      var messageStyle = {
           width: '550px',
           height: '45px',
           float: 'left',
           margin: '5px',
           marginRight: '-30px',
           padding: '3px',
-          border: 'none'
+          border: 'none',
+          borderRadius: '15px'
       };
+      if (this.state.state === HELP_STATES.INITIAL) {
+          messageStyle.visibility = 'hidden';
+      }
 
+/*
       return (
           <div style={containerStyle}>
               <input type="text" style={messageStyle} />
               <button style={helpButtonStyle} onClick={this.clickTransition()}>{this.helpButtonIcon}</button>
           </div>
       );
-    }
+    */
 
+      return (
+          <div style={containerStyle}>
+              <input type="text" value={this.state.message} onChange={this.onMessageChange} style={messageStyle}/>
+              <button style={helpButtonStyle} onClick={this.clickTransition}>{this.helpButtonIcon()}</button>
+          </div>
+      );
+    }
 
 });
